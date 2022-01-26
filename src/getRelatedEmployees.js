74610419cd4d332ed8cffb3d846c6b0450b8a9b5
FlavioVillar <flavio_villar@hotmail.com>
2022-01-26 02:14:54 -0300

    correcao Lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,12 +1,11 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
 
 function isManager(employeeName) {
-console.log(data.employees);
+  console.log(data.employees);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
 
-console.log(isManager("4b40a139-d4dc-4f09-822d-ec25e819a5ad"));
 module.exports = { isManager, getRelatedEmployees };
