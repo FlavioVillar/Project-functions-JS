4f0bf56b0445dab5ef51ed7ca09f43d72693d600
FlavioVillar <flavio_villar@hotmail.com>
2022-01-26 02:10:11 -0300

    requisito 5 corrigido Lint - countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,19 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // bloco if para quando não receber parâmetro, coloca ({}) no reduce para virar objeto, usa spread ... para copiar as informações do array original e colar em outro lugar. [atual.name] - [] usado para criar a key acessando o elemento no array.
+  // bloco if para quando não receber parâmetro, coloca ({}) no reduce para virar objeto, usa spread ...  para copiar as informações do array original e colar em outro lugar. [atual.name] - [] usado para criar a key acessando o elemento no array.
   if (animal === undefined) {
     return data.species.reduce(
       (acc, atual) => ({ ...acc, [atual.name]: atual.residents.length }),
       {},
     );
   }
+  // condição quando só tem o parâmetro do name, sem parâmetro sex.
   if (animal.sex === undefined) {
-    return data.species.find((item) => item.name === animal.specie).residents.length;
+    return data.species.find((item) => item.name === animal.specie).residents
+      .length;
   }
   const animalFiltro = data.species.find((item) => item.name === animal.specie);
   return animalFiltro.residents.reduce(
-    (acc, atual) => (atual.sex === animal.sex ? acc + 1 : acc), 0,
+    (acc, atual) => (atual.sex === animal.sex ? acc + 1 : acc),
+    0,
   );
 }
 
