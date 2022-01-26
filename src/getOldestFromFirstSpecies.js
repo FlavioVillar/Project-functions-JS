d10c1917c16317c57bcb795034822001e7ca37e7
FlavioVillar <flavio_villar@hotmail.com>
2022-01-26 12:44:36 -0300

    requisito 4 OK - getRelatedEmployees

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function speciesByIds(ids) {
+  return data.employees.find((id) => ids.includes(id));
 }
 
-module.exports = getOldestFromFirstSpecies;
+console.log(speciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
+// function getOldestFromFirstSpecies(ids) {
+//   return data.employees
+//     .filter(({ id }) => ids.includes(id))
+//     .map((animal) => `${animal.responsibleFor}`);
+// }
+
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+// module.exports = getOldestFromFirstSpecies;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(employeeName) {
-  console.log(data.employees);
+  return employees.some((manager) => manager.managers.includes(employeeName));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return employees
+      .filter((responsible) => responsible.managers.includes(managerId))
+      .map((funcionario) => `${funcionario.firstName} ${funcionario.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
